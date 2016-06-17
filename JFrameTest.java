3a65bcb79987970ad60edbfcb1f844c98c2c886e
yuzjikowol <runatiru009@yahoo.co.jp>
2016-06-17 11:07:41 +0900

    rennsyuunoJava

---
+++ JFrameTest.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+class JFrameTest extends JFrame{
+ public static void main(String[] args){
+
+ JFrame f = new JFrame();//ƒtƒŒ[ƒ€‰æ–Ê•\¦
+ f.setSize(300,200);
+ f.setVisible(true);
+}
+}
