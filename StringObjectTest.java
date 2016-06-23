01cfed9e2a8009cf3e115aa30a306ada080ec5ee
yuzjikowol <C0222@ITC-C02.local>
2016-06-23 09:18:42 +0900

    オブジェクトの練習

---
+++ StringObjectTest.java
@@ -0,0 +1,14 @@
+class StringObjectTest{
+ public static void main(String[] args){
+
+ char c ="Hello.".charAt(1);
+ 
+ System.out.println("Hello.‚Ì1•¶š–Ú‚Í"+c+"‚Å‚·B");
+ 
+ 	String s2 = "Hello.".toLowerCase();
+ 	System.out.println("Hello.‚ğ¬•¶š‚É‚·‚é‚Æ"+s2+"‚Å‚·B");
+ 	
+ 	String s3 = "Hello.".toUpperCase();
+ 	System.out.println("Hello.‚ğ‘å•¶š‚É‚·‚é‚Æ"+s3+"‚Å‚·B");
+ }
+}
