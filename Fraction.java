76f546d6b2ee9440883dec1ca60c22dcd727df0d
yuzjikowol <C0222@ITC-C02.local>
2016-06-24 12:35:40 +0900

    クラスの練習その1

---
+++ Fraction.java
@@ -0,0 +1,5 @@
+class Fraction{
+   int numerator;
+   int denominator;
+
+}
---
+++ FractionTest1.java
@@ -0,0 +1,14 @@
+class FractionTest1{
+public static void main(String[] args){
+ Fraction f = new Fraction();//æ‚ÉFraction.java‚ğƒRƒ“ƒpƒCƒ‹
+
+
+ f.numerator   =1;
+ f.denominator =2;
+ 
+
+ System.out.println("f=" + f.numerator + "/" + f.denominator);
+ 
+}
+}
+  
