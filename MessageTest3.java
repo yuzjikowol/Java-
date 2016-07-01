528e0a64c26d717f48a3f3aa954c82307ac221d1
yuzjikowol <C0222@ITC-C02.local>
2016-07-01 09:03:31 +0900

    引数その2

---
+++ MessageTest3.java
@@ -0,0 +1,12 @@
+class MessageTest3{
+
+ static void message(String message){
+    System.out.println(message);
+ }
+
+ public static void main(String[] args){
+ message("Good morning.");
+ message("Good afternoon.");
+ message("Good evening.");
+ }
+}
