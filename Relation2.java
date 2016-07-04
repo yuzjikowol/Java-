4af7dd9bbc643f5e00dd075b8e21578d4a7cd16e
yuzjikowol <C0222@ITC-C02.local>
2016-07-04 09:27:45 +0900

    比較演算子

---
+++ Relation2.java
@@ -0,0 +1,10 @@
+class Relation2{
+public static void main(String[] args){
+  System.out.println(1.0<2);
+  System.out.println(1<=2L);
+  System.out.println(1.0f==2L);
+  System.out.println(1.0f!=2L);
+  System.out.println(1.0d>2.0f);
+  System.out.println(1L>2.0);
+ }
+}
