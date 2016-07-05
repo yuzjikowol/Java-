cf79bd2f7b7c44dd83bc170ccf2d67bc0962d8f0
yuzjikowol <C0222@ITC-C02.local>
2016-07-05 09:11:57 +0900

    引数その3

---
+++ SquareTest1.java
@@ -0,0 +1,11 @@
+class SquareTest1{
+
+  static void square(int n){
+   System.out.println(n+"‚Ì“ñæ‚Í"+  n*n +"‚Å‚·");
+}
+public static void main(String[] args){
+       for(int i=0;i<10;i++){
+        square(i);
+        }
+   }
+}
