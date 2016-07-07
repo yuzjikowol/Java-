9bd6bb95a989195a3dc28f09a6c550dccef9c440
yuzjikowol <C0222@ITC-C02.local>
2016-07-07 09:24:53 +0900

    複数の引数

---
+++ Max1.java
@@ -0,0 +1,17 @@
+class Max1{
+
+static void max(int a,int b){
+
+if(a>b){
+  System.out.println(a);
+}else{
+ System.out.println(b);
+}
+}
+
+public static void main(String args[]){
+
+   max(1,2);
+   max(-3,-4);
+}
+}
