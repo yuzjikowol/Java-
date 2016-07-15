44dfbdd90ec83420c7baf3a1adac24fda4081e87
yuzjikowol <C0222@ITC-C02.local>
2016-07-15 09:34:57 +0900

    キーボード入力例題10-02

---
+++ NumericInput2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class NumericInput2{
+  public static void main(String[] args){
+  java.util.Scanner sin = new java.util.Scanner(System.in);
+  int price = sin.nextInt();
+  System.out.println("Å‚İ‰¿Ši‚Í"+ price * 1.05+"‰~‚Å‚·B");
+}
+}
